86964c4f12adac5ef71daa16b2bb7c32f887acde
Burak <burakcarkci@hotmail.com>
2020-01-11 00:19:12 -0500

    Updates

--- CSharp-7-Reverse-String-Char-and-Stack/Program.cs
+++ CSharp-7-Reverse-String-Char-and-Stack/Program.cs
@@ -8,7 +8,7 @@ namespace ReverseString
         {
             //Reverse string with char array
             var reverseString = new StringReverserWithChar();
-            string result1 = reverseString.Reverse("burak ishak kerem");
+            string result1 = reverseString.CharReverse("burak ishak kerem");
             Console.WriteLine(result1);
 
             //Reverse string with stack
--- CSharp-7-Reverse-String-Char-and-Stack/StringReverserWithChar.cs
+++ CSharp-7-Reverse-String-Char-and-Stack/StringReverserWithChar.cs
@@ -4,16 +4,16 @@ namespace ReverseString
 {
     public class StringReverserWithChar
     {
-        public string Reverse(String item)
+        public string CharReverse(string item)
         {
             char[] charArray = item.ToCharArray();
-            string itemReverse = "";
+            string itemReversed = "";
 
             for (int i = charArray.Length - 1; i >= 0; i--)
             {
-                itemReverse = itemReverse + charArray[i];
+                itemReversed = itemReversed + charArray[i];
             }
-            return itemReverse;
+            return itemReversed;
         }
     }
 }
