e1ffb358b4ccfc2b091e3740810ba6aa6fca7b73
Burak <burakcarkci@hotmail.com>
2020-01-12 15:53:31 -0500

    Reversing Queue With Stack

---
+++ CSharp-9-Reversing-Queue-With-Stack/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ReversingQueue
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            var reverse = new ReversingQueueWithStack();
+            reverse.EnQueue(1);
+            reverse.EnQueue(2);
+            reverse.EnQueue(3);
+            reverse.EnQueue(4);
+            reverse.RemoveFromQueue();
+            reverse.AddBackToQueue();
+            reverse.PrintQueue();
+        }
+    }
+}
---
+++ CSharp-9-Reversing-Queue-With-Stack/ReversingQueueWithStack.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReversingQueue
+{
+    public class ReversingQueueWithStack
+    {
+        private readonly Queue<int> _queue = new Queue<int>();
+        private readonly Stack<int> _stack = new Stack<int>();
+
+        public void EnQueue(int num)
+        {
+            _queue.Enqueue(num);
+        }
+
+        public void DeQueue()
+        {
+            if (_queue.Count == 0)
+            {
+                throw new Exception("Queue is already empty!");
+            }
+            else
+            {
+                _queue.Dequeue();
+            }
+        }
+
+        public void PrintQueue()
+        {
+            if (_queue.Count == 0)
+            {
+                throw new Exception("Queue is empty!");
+            }
+            else
+            {
+                foreach (int nums in _queue)
+                {
+                    Console.WriteLine(nums);
+                }
+            }
+        }
+
+        public void RemoveFromQueue()
+        {
+            while (_queue.Count != 0)
+            {
+                _stack.Push(_queue.Dequeue());
+            }
+        }
+
+        public void AddBackToQueue()
+        {
+            while (_stack.Count != 0)
+            {
+                _queue.Enqueue(_stack.Pop());
+            }
+        }
+    }
+}
