0cbacb7cfafc9be55c470c98f15456ada6c45f76
Burak <burakcarkci@hotmail.com>
2020-01-12 19:49:07 -0500

    Queue Implementation With Circular Array

---
+++ CSharp-10-Queue-Implementation-With-Circular-Array/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace QueueWithCircularArray
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            var myQueue = new QueueImplementation(capacity: 5);
+
+            myQueue.Enqueue(1);
+            myQueue.Enqueue(2);
+            myQueue.Enqueue(3);
+            myQueue.Enqueue(4);
+            myQueue.Enqueue(5);
+            myQueue.DeQueue();
+            myQueue.Enqueue(6);
+            myQueue.DeQueue();
+            myQueue.Enqueue(7);
+            myQueue.DeQueue();
+            myQueue.Enqueue(8);
+            myQueue.Peek();
+            myQueue.PrintQueue();
+        }
+    }
+}
---
+++ CSharp-10-Queue-Implementation-With-Circular-Array/QueueImplementation.cs
@@ -0,0 +1,72 @@
+﻿namespace QueueWithCircularArray
+{
+    public class QueueImplementation
+    {
+        private int[] nums;
+        private int front = -1;
+        private int rear = -1;
+
+        public QueueImplementation(int capacity)
+        {
+            nums = new int[capacity];
+        }
+
+        public void Enqueue(int item)
+        {
+            if ((rear + 1) % nums.Length == front)
+            {
+                return;
+            }
+            else if (IsEmpty())
+            {
+                front = rear = 0;
+            }
+            else
+            {
+                rear = (rear + 1) % nums.Length;
+            }
+            nums[rear] = item;
+        }
+
+        public void DeQueue()
+        {
+            if (IsEmpty())
+            {
+                return;
+            }
+            else if (front == rear)
+            {
+                front = rear = -1;
+            }
+            else
+            {
+                front = (front + 1) % nums.Length;
+            }
+        }
+
+        public bool IsEmpty()
+        {
+            if (front == -1 && rear == -1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void Peek()
+        {
+            System.Console.WriteLine("Front of the Queue is --> " + nums[front] + "\n");
+        }
+
+        public void PrintQueue()
+        {
+            foreach (int queue in nums)
+            {
+                System.Console.WriteLine(queue);
+            }
+        }
+    }
+}
