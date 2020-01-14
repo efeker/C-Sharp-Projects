9b6eb59dc8fa6c1bff9134bb474fee4840075560
Burak <burakcarkci@hotmail.com>
2020-01-13 19:36:32 -0500

    Queue Implementation With Two Stacks

---
+++ k-CSharp-Queue-Implementation-With-Two-Stacks/BuildingQueueWithTwoStacks.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QueueWithTwoStacks
+{
+    public class BuildingQueueWithTwoStacks
+    {
+        private readonly Stack<int> stack1 = new Stack<int>();
+        private readonly Stack<int> stack2 = new Stack<int>();
+
+        public void EnQueue(int item)
+        {
+            stack1.Push(item);
+        }
+
+        public int DeQueue()
+        {
+            IsEmpty();
+
+            while (stack1.Count != 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+            return stack2.Pop();
+        }
+
+        public void PrintQueue()
+        {
+            IsEmpty();
+
+            while (stack1.Count != 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+
+            foreach (int nums in stack2)
+            {
+                Console.WriteLine(nums);
+            }
+        }
+
+        public int Peek()
+        {
+            IsEmpty();
+
+            while (stack1.Count != 0)
+            {
+                stack2.Push(stack1.Pop());
+            }
+            return stack2.Peek();
+        }
+
+        public void IsEmpty()
+        {
+            if ((stack2.Count == 0) && (stack1.Count == 0))
+            {
+                throw new Exception("Stack is empty");
+            }
+        }
+    }
+}
---
+++ k-CSharp-Queue-Implementation-With-Two-Stacks/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace QueueWithTwoStacks
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            var myQueue = new BuildingQueueWithTwoStacks();
+            myQueue.EnQueue(1);
+            myQueue.EnQueue(2);
+            myQueue.EnQueue(3);
+
+            //myQueue.PrintQueue();
+
+            //myQueue.DeQueue();
+            //myQueue.DeQueue();
+
+            //var item = myQueue.Peek();
+            //Console.WriteLine(item);
+
+            //var lastItem = myQueue.DeQueue();
+            //Console.WriteLine(lastItem);
+        }
+    }
+}
