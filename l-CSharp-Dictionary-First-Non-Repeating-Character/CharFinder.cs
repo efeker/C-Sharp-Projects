08f2119fabfd8cabca929761c7feb74012ac54e1
Burak <burakcarkci@hotmail.com>
2020-01-16 14:48:04 -0500

    Dictionary/HashTables

---
+++ l-CSharp-Dictionary-First-Non-Repeating-Character/CharFinder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    public class CharFinder
+    {
+        public void FindFirstNonRepeatingChar(string str)
+        {
+            Dictionary<char, int> map = new Dictionary<char, int>();
+
+            foreach (var ch in str)
+            {
+                if (map.ContainsKey(ch))
+                {
+                    map[ch]++;
+                }
+                else
+                {
+                    map.Add(ch, 1);
+                }
+            }
+
+            var pair = map.FirstOrDefault(x => x.Value == 1).Key;
+
+            Console.WriteLine("First non-repeating character: " + pair);
+        }
+    }
+}
---
+++ l-CSharp-Dictionary-First-Non-Repeating-Character/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp3
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var hashmap = new CharFinder();
+            hashmap.FindFirstNonRepeatingChar("consolconsole");
+        }
+    }
+}
