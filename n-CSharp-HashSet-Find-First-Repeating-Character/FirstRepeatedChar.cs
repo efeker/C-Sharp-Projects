974c2a7e1fd18e2890aa14dc69fd84622af99e44
Burak <burakcarkci@hotmail.com>
2020-01-17 15:34:27 -0500

    HashSet Find First Repeating Character

---
+++ n-CSharp-HashSet-Find-First-Repeating-Character/FirstRepeatedChar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace Exercises
+{
+    public class FindRepeatedChar
+    {
+        public char FindFirstRepeatedChar(string item)
+        {
+            HashSet<char> set = new HashSet<char>();
+
+            foreach (var ch in item.ToCharArray())
+            {
+                if (set.Contains(ch))
+                {
+                    return ch;
+                }
+                else
+                {
+                    set.Add(ch);
+                }
+            }
+            return char.MinValue;
+        }
+    }
+}
---
+++ n-CSharp-HashSet-Find-First-Repeating-Character/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercises
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var set = new FindRepeatedChar();
+            var ch = set.FindFirstRepeatedChar("a green apple");
+            Console.WriteLine("First repeated character is: " + ch);
+        }
+    }
+}
