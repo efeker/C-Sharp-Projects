e3684d27de4f00aea5bf2283a4f77782ba4e3e5e
Burak <burakcarkci@hotmail.com>
2020-01-20 01:52:21 -0500

    Priority Queue Implementation

---
+++ p-CSharp-Priority-Queue-Implementation/PriorityQueue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PriorityQueueImplementation
+{
+    public class PriorityQueue
+    {
+        private int[] items;
+        private int count;
+
+        public PriorityQueue(int capacity)
+        {
+            items = new int[capacity];
+        }
+
+        public void Add(int item)
+        {
+            if (count == items.Length)
+            {
+                throw new Exception("Queue is full");
+            }
+            //shifting items
+            int i;
+            for (i = count - 1; i >= 0; i--)
+            {
+                if (items[i] > item)
+                {
+                    items[i + 1] = items[i];
+                }
+                else
+                {
+                    break;
+                }
+            }
+            items[i + 1] = item;
+            count++;
+        }
+
+        public int Remove()
+        {
+            if (IsEmpty())
+            {
+                throw new Exception("Queue is empty!");
+            }
+            return items[--count];
+        }
+
+        public bool IsEmpty()
+        {
+            return (count == 0);
+        }
+
+        public void PrintQueue()
+        {
+            foreach (int nums in items)
+            {
+                Console.WriteLine(nums);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ p-CSharp-Priority-Queue-Implementation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PriorityQueueImplementation
+{
+    internal partial class Program
+    {
+        private static void Main(string[] args)
+        {
+            var myQueue = new PriorityQueue(capacity: 5);
+            myQueue.Add(5);
+            myQueue.Add(3);
+            myQueue.Add(1);
+            myQueue.Add(6);
+            myQueue.Add(4);
+            myQueue.PrintQueue();
+
+            while (!myQueue.IsEmpty())
+            {
+                Console.WriteLine(myQueue.Remove());
+            }
+        }
+    }
+}
